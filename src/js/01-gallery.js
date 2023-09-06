f420ba13401e5dc4167b05360a616aa8afd74e0a
OlenaKovalenko <elenkow17@gmail.com>
2023-09-06 23:05:44 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,54 +7,24 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const container = document.querySelector(".gallery");
-const markup = createMarkup(galleryItems)
+const markup = createMarkup(galleryItems);
 
 container.insertAdjacentHTML('beforeend', markup);
-container.addEventListener('click', onGalleryItemClick);
 
 function createMarkup(arr) {
-    return arr.map(({ preview, original, description }) => {
-        return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
+  return arr.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
 </li>`
     }).join("")
 };
 
-function onGalleryItemClick(event) {
+const lightbox = new SimpleLightbox('.gallery a', { /* options */
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+  });
 
-  event.preventDefault();
-
-  if (event.target === event.currentTarget) {
-    return;
-  }
-
-  const targetElement = event.target;
-  const elementSource = targetElement.dataset.source;
-  
-  const instance = basicLightbox.create(`
-    <img src="${elementSource}" alt="${targetElement.alt}">
-`, {
-    onShow: (instance) => {
-    window.addEventListener('keydown', onEscKeyPress);
-    },
-    onClose: (instance) => {
-    window.removeEventListener('keydown', onEscKeyPress);
-    }
-});
-
-  instance.show();
-
-  function onEscKeyPress(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  }
-}
 console.log(galleryItems);
