4f164763e3629f4fffba5cb5441f2eff7e6ea310
OlenaKovalenko <elenkow17@gmail.com>
2023-09-08 23:43:44 +0300

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+const inputs = document.querySelectorAll("input");
+const inputsArr = [...inputs];
+console.log(inputs);
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+
+const formData = {};
+
+function onFormInput(event) {
+
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateInput(event) {
+    const savedInput = localStorage.getItem(STORAGE_KEY);
+    const parseObject = JSON.parse(savedInput);
+    console.log(savedInput);
+    console.log(parseObject);
+
+    if (savedInput) {
+        for (const key in parseObject) {
+            inputsArr.forEach(input => {
+                input.value = parseObject[key]
+            });
+         
+        }
+    }
+}
+
+populateInput();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const message = formElements.message.value;
+
+    if (mail === "" || message === "") {
+        alert("Please fill in all the fields!");
+        return;
+        
+    } else { 
+        event.currentTarget.reset();
+        localStorage.removeItem(STORAGE_KEY);
+        console.log(formData);
+    }
+}
 
