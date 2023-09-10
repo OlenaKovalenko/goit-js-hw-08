cf77e284ec879145503f9807079016cde0e1e186
OlenaKovalenko <elenkow17@gmail.com>
2023-09-10 15:25:20 +0300

    Update task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,30 @@
-import vimeoPlayer from "@vimeo/player";
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+const STORAGE_KEY = "videoplayer-current-time";
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+player.on('timeupdate', throttle(handleTimeUpdate, 1000));
+
+function handleTimeUpdate(event) {
+    localStorage.setItem(STORAGE_KEY, event.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+
+
+// player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
